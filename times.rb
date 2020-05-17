71d33d72aaebc55656d3264949d3b00bdb880a1f
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 23:24:54 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,11 +1,6 @@
-Wingardium_Leviosa=7
-times do
-   puts "Wingardium Leviosa"
-	 Wingardium_Leviosa = Wingardium_Leviosa-7 
-	end
-
-	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium
-  end 
+def using_times
+	#your code here
+  7.times do
+    puts "Wingardium Leviosa"
+  end
 end
-
-
